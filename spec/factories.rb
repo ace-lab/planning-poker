9026da460a0de6dada1ee0bcc30df7c8239e9726
an-ju <an_ju@berkeley.edu>
2019-02-19 15:38:08 -0800

    Suppressed rubo-cop warnings.

--- spec/factories.rb
+++ spec/factories.rb
@@ -8,9 +8,9 @@ FactoryBot.define do
   end
 
   factory :vote do
-    user      Forgery::Internet.email_address
-    story_id  Forgery::Basic.number(at_least: 1, at_most: 20)
-    vote      Forgery::Basic.number(at_least: 1, at_most: 5)
+    user      { Forgery::Internet.email_address }
+    story_id  { Forgery::Basic.number(at_least: 1, at_most: 20) }
+    vote      { Forgery::Basic.number(at_least: 1, at_most: 5) }
   end
 
 end
